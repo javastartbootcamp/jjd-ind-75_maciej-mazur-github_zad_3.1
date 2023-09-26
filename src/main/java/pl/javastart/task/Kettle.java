9a698a307aa5a43a55305be822350019d9b2ee06
Maciej Mazur <maciej.mazur.mailbox@gmail.com>
2023-09-26 18:57:31 +0200

    Solution for 1.2 task.

--- src/main/java/pl/javastart/task/Kettle.java
+++ src/main/java/pl/javastart/task/Kettle.java
@@ -4,4 +4,10 @@ public class Kettle {
     double capacity;
     String color;
     double netWeight;
+
+    public Kettle(double capacity, String color, double netWeight) {
+        this.capacity = capacity;
+        this.color = color;
+        this.netWeight = netWeight;
+    }
 }
